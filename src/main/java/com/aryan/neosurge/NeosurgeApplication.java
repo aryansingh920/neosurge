f6e7ecb581b4b14dbd8083f7a25cdabc945db909
Aryan Singh <aryansingh920@outlook.com>
2024-04-30 13:52:24 +0530

    init

---
+++ src/main/java/com/aryan/neosurge/NeosurgeApplication.java
@@ -0,0 +1,13 @@
+package com.aryan.neosurge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeosurgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeosurgeApplication.class, args);
+	}
+
+}
